565361f1fdaa486c8b8185fec515118b5f142fda
Mariani Federico <federico.mariani@eng.it>
2018-05-23 20:35:00 +0200

    rel

--- esperimenti-eng/src/main/java/it/croway/esperimenti/Vars.java
+++
@@ -1,8 +0,0 @@
-package it.croway.esperimenti;
-
-public class Vars {
-
-	public static String fileLoc = "/home/federico/Downloads/Consulente-ditta_Inserimento.xlsx";
-	public static int maxUser = 500;
-
-}
